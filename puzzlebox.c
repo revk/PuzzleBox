57b560235810b8a31f058b22368dcadc90446188
RevK <github@e.gg>
2018-12-25 17:45:40 +0000

    Basic 3D side ready for testing, then to make actual maze

--- puzzlebox.c
+++ puzzlebox.c
@@ -17,11 +17,11 @@ main (int argc, const char *argv[])
   double corediameter = 5;
   double coreheight = 50;
   double wallthickness = 2;
-  double mazethickness = 2;
-  double mazestep = 2;
+  double mazethickness = 1;
+  double mazestep = 3;
   double clearance = 0.2;
-  double fn = 0;
-  int walls = 2;
+  int fn = 100;
+  int walls = 4;
   int wall = 0;
   int outside = 0;
   int flat = 0;
@@ -40,7 +40,7 @@ main (int argc, const char *argv[])
       {"maze-thickness", 'd', POPT_ARG_DOUBLE | POPT_ARGFLAG_SHOW_DEFAULT, &mazethickness, 0, "Maze thickness", "mm"},
       {"maze-step", 's', POPT_ARG_DOUBLE | POPT_ARGFLAG_SHOW_DEFAULT, &mazestep, 0, "Maze step", "mm"},
       {"clearance", 'g', POPT_ARG_DOUBLE | POPT_ARGFLAG_SHOW_DEFAULT, &clearance, 0, "Clearance", "mm"},
-      {"fn", 'a', POPT_ARG_DOUBLE, &fn, 0, "$fn", "N"},
+      {"fn", 'a', POPT_ARG_INT | POPT_ARGFLAG_SHOW_DEFAULT, &fn, 0, "$fn", "N"},
       POPT_AUTOHELP {}
     };
 
@@ -116,7 +116,7 @@ main (int argc, const char *argv[])
 	      clearance = value;
 	      break;
 	    case 'a':
-	      fn = value;
+	      fn = (int) value;
 	      break;
 	    }
 	}
@@ -137,28 +137,87 @@ main (int argc, const char *argv[])
   if (flat)
     printf ("// Non helical maze\n");
   if (fn)
-    printf ("$fn=%f;\n", fn);
+    printf ("$fn=%d;\n", fn);
   // The nub
-  printf ("module nub(){cube([%f,%f,%f]);}\n", mazestep / 2, mazestep / 2, mazethickness);
+  //printf ("module nub(){rotate([%d,0,0])hull(){translate([0,0,%f])cylinder(d=%f,h=%f,$fn=8);cylinder(d1=%f,d2=%f,h=%f,$fn=8);}}\n", outside ? 90 : -90, -mazethickness, mazestep * 2 / 3, mazethickness / 2, mazestep * 2 / 3, mazestep / 3, mazethickness);
+  printf ("module nub(){rotate([90,0,0])translate([0,0,%f])cylinder(d1=%f,d2=%f,h=%f,$fn=4);}\n", -mazethickness, mazestep, mazestep / 3, mazethickness * 2);
   double x = 0;
   void box (int wall)
   {				// Make the box - wall 1 in inside
-    printf ("// Wall %d\n", wall);
     // Dimensions
-    double r = corediameter / 2 + wallthickness + (wall - 1) * (wallthickness + mazethickness);	// radius inside before maze added (or for outer)
-    double height = coreheight + wallthickness;
-
-    printf ("translate([%f,0,0]){\n", x + r + (wall < walls ? mazethickness : 0));
-    // Make the unit
+    double r1 = corediameter / 2 + wallthickness + (wall - 1) * (wallthickness + mazethickness + clearance);
+    double r0 = r1 - wallthickness;
+    double r2 = r1;
     if (wall < walls)
-      printf ("cylinder(r=%f,h=%f);\n", r + mazethickness, baseheight);
-    printf ("cylinder(r=%f,h=%g);\n", r, height);
-
-    // Create the maze
-
-    // Punch the maze
+      r2 += wallthickness + mazethickness + clearance;
+    if (outside && wall < walls)
+      {				// Allow for maze on outside
+	r1 += mazethickness;
+	r2 += mazethickness;
+      }
+    if (!outside && wall > 1)
+      {				// Allow for maze on inside
+	r0 -= mazethickness;
+      }
+    double height = coreheight + wallthickness * wall;
+    if (wall > 1)
+      height -= baseheight;
+    // Output
+    printf ("// Wall %d\n", wall);
+    // Maze dimensions
+    double r = (outside ? r1 : r0);
+    double y = (outside ? baseheight : wallthickness);
+    double h = height - y;
+    double w = r * 2 * PI;
+    int H = (int) (h / mazestep) + 1;
+    int W = (int) (w / mazestep) / 2 * 2;
+    double a = 0, dy = 0;
+    if (!flat)
+      {
+	a = atan (mazestep / w);
+	dy = mazestep / W;
+      }
+    void nub (int X, int Y, char *t)
+    {
+      printf ("rotate([0,0,%f])translate([0,0,%f])nub%d%s();\n", (double) X * 360 / W, Y * mazestep + y + dy * X, wall, t);
+    }
+    printf ("module nub%d(){rotate([0,%f,0])translate([0,%f,0])nub();}\n", wall, a, r + clearance / 2);
+    printf ("module nub%dx(){hull(){nub%d();rotate([0,0,%f])translate([0,0,%f])nub%d();}hull(){rotate([0,0,%f])translate([0,0,%f])nub%d();rotate([0,0,%f])translate([0,0,%f])nub%d();}}\n", wall, wall, (double) 360 / W / 2, dy / 2, wall, (double) 360 / W / 2, dy / 2, wall, (double) 360 / W, dy, wall);
+    printf ("module nub%dy(){hull(){nub%d();translate([0,0,%f])nub%d();}}\n", wall, wall, mazestep, wall);
+    printf ("translate([%f,0,0]){\n", x + r2);
+    printf ("difference(){\n");
+    if (r2 > r1)
+      printf ("union(){cylinder(r=%f,h=%f);cylinder(r=%f,h=%f);}\n", r2, baseheight, r1, height);
+    else
+      printf ("cylinder(r=%f,h=%f);\n", r1, height);
+    printf ("translate([0,0,%f])cylinder(r=%f,h=%f);\n", wallthickness, r0, height);
+    if ((outside && wall < walls) || (!outside && wall > 1))
+      {				// Maze cut out
+	// Make maze
+	char maze[W][H];
+	memset (maze, 0, W * H);
+	// components
+	// Cut maze
+	int X, Y;
+	for (Y = 0; Y < H; Y++)
+	  {
+	    printf ("render(){\n");
+	    for (X = 0; X < W; X++)
+	      {
+		nub (X, Y, "x");
+		nub (X, Y, "y");
+	      }
+	    printf ("}\n");
+	  }
+      }
     printf ("}\n");
-    x += (r + (wall < walls ? mazethickness : 0)) * 2 + 10;
+    if ((outside && wall > 1) || (!outside && wall < walls))
+      {
+	double rn = (outside ? r0 : r2);
+	printf ("translate([0,%f,%f])nub();rotate([0,0,180])translate([0,%f,%f])nub();\n", rn, height - mazestep, rn, height - mazestep - (flat ? 0 : mazestep / 2));
+      }
+    printf ("}\n");
+    x += r2 * 2 + 10;
   }
   if (wall)
     box (wall);
