c4656cce480421684ea675f413cdf6f0a7a3dc48
RevK <github@e.gg>
2018-12-26 18:08:23 +0000

    More tweaks

--- puzzlebox.c
+++ puzzlebox.c
@@ -25,7 +25,7 @@ main (int argc, const char *argv[])
   double wallthickness = 1.8;
   double mazethickness = 1.8;
   double mazestep = 3;
-  double clearance = 0.3;
+  double clearance = 0.5;
   double coregap = 0;
   int curvesteps = 100;
   int walls = 4;
